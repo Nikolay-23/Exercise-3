195406a789988df3375e3d3d5319b8570701f1ab
4KT <4KT@DESKTOP-J0KOTS8>
2024-11-11 02:09:01 +0000

    Add project files.

---
+++ Задача-3/Задача-3.cpp
@@ -0,0 +1,15 @@
+﻿
+#include <iostream>
+
+int main()
+{
+	int all = 0;
+    int num;
+    scanf_s("%d", &num);
+
+	for (int i = 0; i <= num; i++)
+	{
+		all = pow(2, i);
+		printf("%d ", all);
+	}
+}
